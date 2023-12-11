7a1f8cc54e430af12174585cc0d6943fb0b8a443
Begum V <bgm35gcr@gmail.com>
2023-12-10 23:14:37 -0800

    styles added and updated

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -3,21 +3,42 @@ import { ShopContext } from '../../context/shop-context'
 
 export const Product = (props) => {
     const { id, productName, price, productImage } = props.data
-    const { addToCart, cartItems } = useContext(ShopContext)
+    const { addToCart, removeFromCart, cartItems } = useContext(ShopContext)
 
     const cartItemAmount = cartItems[id]
+
+    const handleAddToCart = () => {
+        addToCart(id)
+    }
+
+    const handleRemoveFromCart = () => {
+        removeFromCart(id)
+    }
+
     return (
         <div className="product">
-            <img src={productImage} />
+            <img src={productImage} alt={productName} />
             <div className="description">
                 <p>
                     <b>{productName}</b>
                 </p>
                 <p>${price}</p>
             </div>
-            <button className="addToCartBttn" onClick={() => addToCart(id)}>
-                {`Add To Cart ${cartItemAmount > 0 ? cartItemAmount : 0}`}
-            </button>
+            <div className="cartActions">
+                {cartItemAmount > 0 && (
+                    <button
+                        className="removeFromCartBttn"
+                        onClick={handleRemoveFromCart}
+                    >
+                        <span role="img" aria-label="Remove">
+                            ❌
+                        </span>
+                    </button>
+                )}
+                <button className="addToCartBttn" onClick={handleAddToCart}>
+                    Add To Cart{cartItemAmount > 0 && <> ({cartItemAmount})</>}
+                </button>
+            </div>
         </div>
     )
 }
