1d9f3b96c5b3e6efcccaee5577b9dd90b543ce3b
Begum V <bgm35gcr@gmail.com>
2023-12-10 23:27:46 -0800

    cart icon updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { ShoppingCart } from 'phosphor-react'
+import { ShopContext } from '../context/shop-context'
 import './navbar.css'
 
 export const Navbar = () => {
+    const { cartItems } = useContext(ShopContext)
+
+    // Calculate the total number of items in the cart
+    const cartItemCount = Object.values(cartItems).reduce(
+        (total, count) => total + count,
+        0
+    )
+
     return (
         <div className="navbar">
             <div className="links">
-                <Link to="/"> Shop</Link>
+                <Link to="/">Shop</Link>
                 <Link to="/cart">
                     <ShoppingCart size={32} />
+                    {cartItemCount > 0 && (
+                        <span className="cart-badge">{cartItemCount}</span>
+                    )}
                 </Link>
             </div>
         </div>
